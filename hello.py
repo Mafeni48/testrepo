69548ea95d9e9546fc71ac0018a15125a676dc6f
Mafeni48 <mafeni48@gmail.com>
2025-01-25 12:23:24 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+def main():
+    name = input ("Whats your name ").strip().title()
+    hello(name)
+
+def hello(to=""):
+    print("hello,", to)
+# Ask user for your name
+# hello()
+
+
+
+main()
+
+ 
